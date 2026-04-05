4682bac6874fc4fc657fa02ac7cdffee1d944f86
AminaChe <161376172+AminaChe@users.noreply.github.com>
2026-04-05 16:18:10 +0200

    refactor: remove duplication in trimFrom by delegating to trimLeadingFrom and trimTrailingFrom

--- guava/src/com/google/common/base/CharMatcher.java
+++ guava/src/com/google/common/base/CharMatcher.java
@@ -761,22 +761,7 @@ public abstract class CharMatcher implements Predicate<Character> {
    * ... is equivalent to {@link String#trim()}.
    */
   public String trimFrom(CharSequence sequence) {
-    int len = sequence.length();
-    int first;
-    int last;
-
-    for (first = 0; first < len; first++) {
-      if (!matches(sequence.charAt(first))) {
-        break;
-      }
-    }
-    for (last = len - 1; last > first; last--) {
-      if (!matches(sequence.charAt(last))) {
-        break;
-      }
-    }
-
-    return sequence.subSequence(first, last + 1).toString();
+    return trimTrailingFrom(trimLeadingFrom(sequence));
   }
 
   /**
